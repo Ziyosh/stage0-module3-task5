45a5b52a482f62e97c7eb98de6498664a96792a6
Ziyosh <ziyodaakhmedova1998@gmail.com>
2022-07-28 16:31:29 +0300

    Last 3.5 commit

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -3,15 +3,10 @@ package lang.print.gaps.task5;
 import java.util.Enumeration;
 
 public class NumberDivider {
-    static void divide(int toBeDivided){
-            int num;
-            toBeDivided = 19;
-            num = toBeDivided / 5;
-            System.out.println(num);
-        }
-        public static void main(String[] args){
-
-        divide((int) 3.8);
-        }
+    static void divide(int toBeDivided) {
+        double num;
+        num = (double)toBeDivided/5;
+        System.out.println(num);
     }
+}
 
